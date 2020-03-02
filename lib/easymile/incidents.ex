bcd0e723fadff843cd5c03b38967d0e149186a2e
Timothy Regan <tregan@pillartechnology.com>
2020-03-02 09:09:54 -0500

    Context for incidents

--- lib/easymile/incidents.ex
+++ lib/easymile/incidents.ex
@@ -2,42 +2,26 @@ NimbleCSV.define(CsvParser, [])
 
 defmodule Reports.Easymile.Incidents do
   def run(params \\ []) do
-    # data = query(params)
-    # |> Stream.map(fn row -> for {key, val} <- row, into: %{}, do: {String.to_atom(key), val} end)
-    # |> Enum.into([])
-
-    data =
-      # "#{__MODULE__}.download.csv"
-      "test.csv"
-      |> File.stream!(read_ahead: 100_000)
-      |> CsvParser.parse_stream()
-      |> Stream.map(fn [id, last_seen, lat, lon, mode, reason, speed] ->
-        %{
-          id: id,
-          last_seen: date_parse(last_seen),
-          lat: lat,
-          lon: lon,
-          mode: mode,
-          reason: reason,
-          speed: speed
-        }
-      end)
-      |> Enum.into([])
+    data = query(params)
+    |> Stream.map(fn row -> for {key, val} <- row, into: %{}, do: {String.to_atom(key), val} end)
+    |> Stream.map(fn row -> Map.update!(row, :last_seen, &date_parse/1) end)
+    |> Enum.into([])
 
     IO.inspect("Got data")
 
     incidents =
       data
-      |> Enum.reduce({[], nil}, fn current, {incidents, last} ->
+      |> Enum.reduce({[], [], nil}, fn current, {incidents, context, last} ->
         if last && current.reason != last.reason && current.id == last.id do
           incident =
             current
             |> Map.put(:reason_pre, last.reason)
+            |> Map.put(:context, context |> Enum.take(10))
             |> Map.delete(:speed)
 
-          {[incident | incidents], current}
+          {[incident | incidents], [], current}
         else
-          {incidents, current}
+          {incidents, [current | context], current}
         end
       end)
       |> elem(0)
@@ -48,7 +32,7 @@ defmodule Reports.Easymile.Incidents do
 
     incidents_with_context =
       incidents
-      |> Enum.map(fn incident -> get_context(incident, data) end)
+      |> Enum.map(fn incident -> get_context(incident) end)
 
     IO.inspect("Got incident contexts")
 
@@ -66,19 +50,16 @@ defmodule Reports.Easymile.Incidents do
     File.write!("#{__MODULE__}.csv", headers ++ report)
   end
 
-  defp get_context(incident, data) do
+  defp get_context(incident) do
     IO.puts("Getting context for incident at: #{incident.last_seen}")
     incident_time = incident.last_seen
-    incident_bound = DateTime.add(incident_time, -6, :second) |> IO.inspect(label: "incidents.ex:71")
 
     highest_speed_datum =
-      Enum.filter(data, fn datum ->
-        (incident_time > datum.last_seen) |> IO.inspect(label: "prior to incident")
-        (datum.last_seen >= incident_bound) |> IO.inspect(label: "before cutoff")
-        (incident_time > datum.last_seen and datum.last_seen >= incident_bound)
+      Enum.filter(incident.context, fn datum ->
+        time_since_incident = NaiveDateTime.diff(incident_time, datum.last_seen)
+        (time_since_incident > 0 and time_since_incident <= 6)
       end)
       |> Enum.sort(fn a, b -> a.speed > b.speed end)
-      |> IO.inspect(label: "incidents.ex:79")
       |> List.first()
 
     if highest_speed_datum do
@@ -143,5 +124,11 @@ defmodule Reports.Easymile.Incidents do
     hours = Keyword.get(params, :hours, 24)
 
     "SELECT attributes.disengagement_reason as reason, attributes.last_seen, attributes.lat, attributes.lon, attributes.speed, attributes.mode, id FROM easymile__linden_states order by id desc, attributes.last_seen asc"
+  end
+
+  defp pmap(collection, func) do
+    collection
+    |> Enum.map(&(Task.async(fn -> func.(&1) end)))
+    |> Enum.map(fn task -> Task.await(task, 15000) end)
   end
 end
